6279735ff6567c3435b2d31305e3963b99273350
nathanielzacharias <nathanielzacharias@gmail.com>
2022-08-29 13:57:15 +0800

    feat: MVP working, unable to set dotenv process, unable to read data at line24 in promise during first click

--- giphy/src/App.js
+++ giphy/src/App.js
@@ -5,18 +5,41 @@ import Form_button from './components/form_button/form_button';
 import Title_header from './components/title_header/title_header.jsx'
 
 function App() {
-  let returnedData = ""
-  let gifURL = ""
+  // let returnedData = ""
+  // let gifURL = ""
+
+  const [returnedData, setReturnedData] = useState("")
+  const [gifURL, setGifURL] = useState("")
+
+  console.log('returnedData1 :', returnedData)
+  console.log('gifURL1 :', gifURL)
+
+  const sendRequest = async () => {
+    // props.returnedData = await axios.get(`${process.env.GIPHY_API_request}`);
+    // props.gifURL = props.returnedData.data.data.images.original.url;
+
+    // setReturnedData(await axios.get(`${process.env.GIPHY_API_request}`))
+    setReturnedData(await axios.get(`https://api.giphy.com/v1/gifs/random?api_key=KGSl8OH7mlaXczeymeh8mG8ABxy7vAg2&tag=&rating=g`))
+    console.log('returnedData2 :', returnedData)
+    setGifURL(returnedData.data.data.images.original.url)
+    console.log('gifURL2 :', gifURL)
+}
 
   return (
     <div className="container">
+
       <Title_header />
-      <Form_button returnedData="" gifURL="" />
+
+      {/* <Form_button returnedData setReturnedData gifURL setGifURL/> */}
+      <button onClick={sendRequest}>Gimme random</button>
+
       <div>
         <img src={gifURL}></img>
       </div>
+
       {console.log('returnedData :', returnedData)}
       {console.log('gifURL :', gifURL)}
+
     </div>
   );
 }
