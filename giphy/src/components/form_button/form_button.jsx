ed458be68c16f518377ae7f253944b3fd7a00ea9
nathanielzacharias <nathanielzacharias@gmail.com>
2022-08-29 11:44:20 +0800

    feat: added component title_header and form_button

---
+++ giphy/src/components/form_button/form_button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Form_button() {
+  return (
+    <div className="container">
+        <form action="/" method="post"> 
+            <button type="submit">Gimme random</button>
+        </form>
+
+    </div>
+  )
+}
+
+export default Form_button
---
+++ giphy/src/components/title_header/title_header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Title_header() {
+  return (
+    <div>
+        <h1>
+            Return a GIF
+        </h1>
+    </div>
+  )
+}
+
+export default Title_header
